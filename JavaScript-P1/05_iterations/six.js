d29bfa6b3d3b221d085b460f65e6f07190a0d10a
Niraj Yadav <nirajyadav9562490@gmail.com>
2026-08-21 12:32:07 +0530

    Revision

--- JavaScript-P1/05_iterations/six.js
+++ JavaScript-P1/05_iterations/six.js
@@ -1,4 +1,4 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+//  const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
 // const values = coding.forEach( (item) => {
@@ -22,7 +22,7 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     }
 // } )
 
-// console.log(newNums);
+//console.log(newNums);
 
 
 const books = [
